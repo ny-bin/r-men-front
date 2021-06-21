634ed33fbda8efba9cf4191cf1f1b5f3b78e46a6
bin <ny.bin1210@gmail.com>
2021-06-21 16:04:41 +0900

    ✨ apollo client の実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../apollo/apolloClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = useApollo(pageProps);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 export default MyApp;
