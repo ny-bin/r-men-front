6ef271a8f8640ab2ddb7b0314738259451a33ee5
bin <ny.bin1210@gmail.com>
2021-07-04 14:44:28 +0900

    ⚡️ firebase functionsの修正→deploy済み

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,34 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import fetch from 'node-fetch';
+import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  gql,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 
 admin.initializeApp();
+
+// firebaseにuser作成時、hasuraにもユーザーを作成
+const httpLink = createHttpLink({
+  uri: functions.config().hasura.url,
+  fetch: fetch as any,
+});
+const authLink = setContext(async (_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'x-hasura-admin-secret': functions.config().hasura.admin_secret,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export const setCustomClaims = functions.auth.user().onCreate(async (user) => {
   const custonClaims = {
@@ -15,6 +42,23 @@ export const setCustomClaims = functions.auth.user().onCreate(async (user) => {
   try {
     await admin.auth().setCustomUserClaims(user.uid, custonClaims);
 
+    await client.mutate({
+      variables: {
+        id: user.uid,
+        name: user.displayName,
+      },
+      mutation: gql`
+        mutation createUser($id: String!, $name: String!) {
+          insert_users_one(object: { id: $id, name: $name }) {
+            id
+            name
+            created_at
+          }
+        }
+      `,
+    });
+
+    //初回ログインのユーザー作成とカスタムクレームの追加には遅延があるのでrefreshタイムの設定をする
     await admin.firestore().collection('user-meta').doc(user.uid).create({
       refreshTime: admin.firestore.FieldValue.serverTimestamp(),
     });
