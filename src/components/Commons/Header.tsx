f6004db69a0bcea0b43120db7e007fc52d8b7b9d
bin <ny.bin1210@gmail.com>
2021-07-07 23:27:35 +0900

    💄 headerの仮作成

--- src/components/Commons/Header.tsx
+++ src/components/Commons/Header.tsx
@@ -1,5 +1,11 @@
 import React, { VFC } from 'react';
 
 export const Header: VFC = () => {
-  return <header></header>;
+  return (
+    <header className="bg-yellow-400 h-16">
+      <div className="float-left w-1/2 text-2xl pl-12 text-red-400 h-full flex content-center flex-wrap">
+        <p className="text-left font-bold">R-MEN</p>
+      </div>
+    </header>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ import { addApolloState, initializeApollo } from '../apollo/apolloClient';
 import { GET_PREFECTURES } from '../apollo/queries/prefectureQueries';
 import { GET_CATEGORIES } from '../apollo/queries/categoryQueries';
 
-import { Layout } from '../components/Commons/Layout';
+import { Footer } from '../components/Commons/Footer';
 import { AreaPrefecture } from '../components/Uncommons/AreaPrefecture';
 
 const Home: VFC = () => {
@@ -29,66 +29,76 @@ const Home: VFC = () => {
     return <div>categoryError:{categoryError.message}</div>;
   }
   return (
-    <Layout title="R-MEN">
-      <div className="bg-r-men-img w-full object-contain bg-contain xl:bg-cover pb-14">
-        <div className="md:w-1/3 z-10 h-64">
-          <div className="grid grid-cols-3 place-content-center h-16 py-5">
-            <Link href="/">
-              <a className="text-gray-700 text-center px-4 py-2 m-2 font-medium">
-                Login
-              </a>
-            </Link>
-            <Link href="/auth/signup">
-              <a className="text-gray-700 text-center px-4 py-2 m-2 font-medium">
-                Sign Up
-              </a>
-            </Link>
-            <Link href="/">
-              <a className="text-gray-700 text-center px-4 py-2 m-2 font-medium">
-                Guest
-              </a>
-            </Link>
-          </div>
+    <>
+      <Head>
+        <title>R-MEN</title>
+        <meta
+          name="description"
+          content="ラーメン専門の検索アプリです。各地域、カテゴリ、ランキング等で絞り込むことができます。"
+        />
+      </Head>
+      <main className="bg-yellow-100 h-auto">
+        <div className="bg-r-men-img w-full object-contain bg-contain xl:bg-cover pb-14">
+          <div className="md:w-1/3 z-10 h-64">
+            <div className="grid grid-cols-3 place-content-center h-16 py-5">
+              <Link href="/auth/signin">
+                <a className="text-gray-700 text-center px-4 py-2 m-2 font-medium">
+                  Login
+                </a>
+              </Link>
+              <Link href="/auth/signup">
+                <a className="text-gray-700 text-center px-4 py-2 m-2 font-medium">
+                  Sign Up
+                </a>
+              </Link>
+              <Link href="/">
+                <a className="text-gray-700 text-center px-4 py-2 m-2 font-medium">
+                  Guest
+                </a>
+              </Link>
+            </div>
 
-          <div className="pt-2">
-            <p className="text-5xl text-red-400 font-serif text-center">
-              R-MEN
-            </p>
-            <p className="text-2xl font-bold text-black-400 font-serif text-center py-4">
-              find your best r-men
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="container mx-auto py-10">
-        <h1 className="mx-auto text-4xl text-center font-serif ">
-          おすすめから探す
-        </h1>
-        <div className="grid grid-cols-2 place-content-center h-16 py-5 my-5">
-          <p className="text-center mb-5">各エリアランキングTOP20</p>
-          <p className="text-center mb-5">ユーザーおすすめ一覧</p>
-          <p className="text-center">SNSで話題のラーメン</p>
-          <p className="text-center">運営おすすめ</p>
-        </div>
-      </div>
-      <div className="container mx-auto pb-10">
-        <h1 className="mx-auto text-4xl text-center font-serif">
-          カテゴリから探す
-        </h1>
-        <div className="grid grid-cols-6 h-auto py-5 my-5 w-2/3 mx-auto">
-          {categoryNameList?.categories?.map((category) => {
-            return (
-              <p className="text-center py-2" key={category.id}>
-                {category.name}
+            <div className="pt-2">
+              <p className="text-5xl text-red-400 font-serif text-center">
+                R-MEN
               </p>
-            );
-          })}
+              <p className="text-2xl font-bold text-black-400 font-serif text-center py-4">
+                find your best r-men
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <AreaPrefecture />
-    </Layout>
+        <div className="container mx-auto py-10">
+          <h1 className="mx-auto text-4xl text-center font-serif ">
+            おすすめから探す
+          </h1>
+          <div className="grid grid-cols-2 place-content-center h-16 py-5 my-5">
+            <p className="text-center mb-5">各エリアランキングTOP20</p>
+            <p className="text-center mb-5">ユーザーおすすめ一覧</p>
+            <p className="text-center">SNSで話題のラーメン</p>
+            <p className="text-center">運営おすすめ</p>
+          </div>
+        </div>
+        <div className="container mx-auto pb-10">
+          <h1 className="mx-auto text-4xl text-center font-serif">
+            カテゴリから探す
+          </h1>
+          <div className="grid grid-cols-6 h-auto py-5 my-5 w-2/3 mx-auto">
+            {categoryNameList?.categories?.map((category) => {
+              return (
+                <p className="text-center py-2" key={category.id}>
+                  {category.name}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+
+        <AreaPrefecture />
+      </main>
+      <Footer />
+    </>
   );
 };
 
