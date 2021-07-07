e9c6ed40503f82878f4c14fec9530b4a7cce7279
bin <ny.bin1210@gmail.com>
2021-07-07 23:25:15 +0900

    💄 UIの修正

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { VFC } from 'react';
 
+import Link from 'next/link';
+
 import { Layout } from '../../components/Commons/Layout';
 import { Auth } from '../../components/Uncommons/Auth';
 
 const SignUp: VFC = () => {
   return (
-    <Layout title="Home">
+    <Layout title="signin">
       <h1 className="text-center text-lg font-semibold pt-8 pb-4 tracking-wider">
-        新規会員登録
+        ログイン
       </h1>
-      <Auth isLogin={false}></Auth>
+      <Auth isLogin={true}></Auth>
+      <div className="h-100">test</div>
     </Layout>
   );
 };
