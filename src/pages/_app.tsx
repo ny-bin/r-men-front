f5cb111929971dad81dde127dff7ceeb8e497168
bin <ny.bin1210@gmail.com>
2021-07-13 11:28:12 +0900

    🐛 ユーザー情報が変更された場合の取得処理を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { initializeApollo, useApollo } from '../apollo/apolloClient';
+import { useApollo } from '../apollo/apolloClient';
+import { useUserChanged } from 'src/hooks/useUserChanged';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const {} = useUserChanged();
   const client = useApollo(pageProps);
   return (
     <ApolloProvider client={client}>
